8df19cef584eb25f43c329c5b4c997283e7e3223
Celso Mireles <celso@digitalstrategy.tips>
2016-06-06 23:12:43 -0700

    Implement i18n
    Couldn’t find a suitable jQuery plugin, so I hacked together this
    method of adding i18n to a static html site. Reminder to make a blog
    post about it soon.

--- _harp/js/main.js
+++ _harp/js/main.js
@@ -1,8 +1,37 @@
 $(document).ready(function(){
 
-		 $('#lang-toggle').bootstrapToggle('on')
+		 $('#lang-toggle').bootstrapToggle('on');
+		 setLanguage('Spanish');
 
 		 $('#lang-toggle').change(function(){
-			 console.log("Language Toggled!");
+			 var isChecked = $(this).prop('checked');
+			 var whichLang = getLanguage(isChecked);
+
+			 setLanguage(whichLang);
+
 		 });
+
+
 });
+
+function getLanguage(status){
+	if (status) {
+		return 'Spanish';
+	} else {
+		return 'English';
+	}
+}
+
+function setLanguage(lang) {
+	$('.i18n').each(function(){
+		switch (lang) {
+		case 'Spanish':
+			var text = $(this).data('spanish');
+			$(this).html(text);
+			break;
+		case 'English':
+			var text = $(this).data('english');
+			$(this).html(text);
+	 }
+	});
+}
