919b1ed3250f1ccd2660ebfb779973dc411235c3
Celso Mireles <celso@digitalstrategy.tips>
2016-06-07 16:24:36 -0700

    Make setLanguage function change images too.

--- _harp/js/main.js
+++ _harp/js/main.js
@@ -1,15 +1,23 @@
 $(document).ready(function(){
+		// Make sure language toggle starts out in Spanish
+		$('#lang-toggle').bootstrapToggle('on');
+		// Set i18n to Spanish
+		setLanguage('Spanish');
 
-		 $('#lang-toggle').bootstrapToggle('on');
-		 setLanguage('Spanish');
 
-		 $('#lang-toggle').change(function(){
-			 var isChecked = $(this).prop('checked');
-			 var whichLang = getLanguage(isChecked);
+		// Run this whenever the language toggle is switched
+		$('#lang-toggle').change(function(){
+			// This is just to get language data in the form of a string.
+			// I know it would be easier to just use the boolean value,
+			// but this is a way to prepare for more languages in the
+			// future, even that would mean using something other
+			// than the toggle as the UI component to select language.
+			var isChecked = $(this).prop('checked');
+			var whichLang = getLanguage(isChecked);
 
-			 setLanguage(whichLang);
+			setLanguage(whichLang);
 
-		 });
+		});
 
 
 });
@@ -23,6 +31,7 @@ function getLanguage(status){
 }
 
 function setLanguage(lang) {
+	// This makes sure all .i18n content is using the set language
 	$('.i18n').each(function(){
 		switch (lang) {
 		case 'Spanish':
@@ -32,6 +41,27 @@ function setLanguage(lang) {
 		case 'English':
 			var text = $(this).data('english');
 			$(this).html(text);
+			break;
 	 }
 	});
+
+	// This makes sure that all images being referenced are using the correct
+	// src attribute
+	$('.img-i18n').each(function() {
+		var self = $(this);
+		switch (lang) {
+			case 'Spanish':
+				var src = self.data('spanish');
+				self.attr('src', src);
+				break;
+			case 'English':
+				var src = self.data('english');
+				self.attr('src', src);
+				break;
+		}
+	});
+
+
+	// This makes sure that all scrips (right now only one) are using the
+	// the current language
 }
