dbed28e2df920542aa30b3b99ec8a5c64b4cbf8c
Celso Mireles <celso@digitalstrategy.tips>
2016-06-28 13:37:51 -0700

    Make smooth scroller offset better.
    Now it scrolls to appropriately without hiding the heading under the
    nav.

--- _harp/public/js/freelancer.js
+++ _harp/public/js/freelancer.js
@@ -9,7 +9,7 @@ $(function() {
     $('body').on('click', '.page-scroll a', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
+            scrollTop: $($anchor.attr('href')).offset().top - 80
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
